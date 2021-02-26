377380affc7ff6dea7ab324fcb7f2099c023ebd0
Josh Jacobson <49420704+jojadev@users.noreply.github.com>
2021-02-26 06:42:58 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+print("OK, rockstar. Let's get your band a name.")
+user_city = input("What is your hometown?\n").capitalize()
+user_pet_name = input("What is the name of your favourite pet?\n").capitalize()
+print("OK, let's call you" + " " + "the" + " " + user_city + " " + user_pet_name + "s")
